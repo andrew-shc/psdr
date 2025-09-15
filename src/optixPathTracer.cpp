d98112ab1aa8970ed650b51ff833832806925e8e
Andrew Shen <andrewshen31@gmail.com>
2025-09-15 13:02:53 -0700

    omitted albedo opt + prep for geo opt

--- src/optixPathTracer.cpp
+++ src/optixPathTracer.cpp
@@ -190,7 +190,7 @@ struct PathTracerState
 const int32_t TRIANGLE_COUNT = 32;
 const int32_t MAT_COUNT = 5;
 
-const static std::array<Vertex, TRIANGLE_COUNT * 3> g_vertices =
+const static std::array<Vertex, TRIANGLE_COUNT * 3> g_vertices_gt =
     {{// Floor  -- white lambert
       {0.0f, 0.0f, 0.0f, 0.0f},
       {0.0f, 0.0f, 559.2f, 0.0f},
@@ -326,6 +326,145 @@ const static std::array<Vertex, TRIANGLE_COUNT * 3> g_vertices =
       {213.0f, 548.6f, 332.0f, 0.0f},
       {343.0f, 548.6f, 332.0f, 0.0f}}};
 
+const float y_pos_param_init = -300.0f; // 35.0f;
+
+const static std::array<Vertex, TRIANGLE_COUNT * 3> g_vertices_init =
+    {{// Floor  -- white lambert
+      {0.0f, 0.0f, 0.0f, 0.0f},
+      {0.0f, 0.0f, 559.2f, 0.0f},
+      {556.0f, 0.0f, 559.2f, 0.0f},
+      {0.0f, 0.0f, 0.0f, 0.0f},
+      {556.0f, 0.0f, 559.2f, 0.0f},
+      {556.0f, 0.0f, 0.0f, 0.0f},
+
+      // Ceiling -- white lambert
+      {0.0f, 548.8f, 0.0f, 0.0f},
+      {556.0f, 548.8f, 0.0f, 0.0f},
+      {556.0f, 548.8f, 559.2f, 0.0f},
+
+      {0.0f, 548.8f, 0.0f, 0.0f},
+      {556.0f, 548.8f, 559.2f, 0.0f},
+      {0.0f, 548.8f, 559.2f, 0.0f},
+
+      // Back wall -- white lambert
+      {0.0f, 0.0f, 559.2f, 0.0f},
+      {0.0f, 548.8f, 559.2f, 0.0f},
+      {556.0f, 548.8f, 559.2f, 0.0f},
+
+      {0.0f, 0.0f, 559.2f, 0.0f},
+      {556.0f, 548.8f, 559.2f, 0.0f},
+      {556.0f, 0.0f, 559.2f, 0.0f},
+
+      // Right wall -- green lambert
+      {0.0f, 0.0f, 0.0f, 0.0f},
+      {0.0f, 548.8f, 0.0f, 0.0f},
+      {0.0f, 548.8f, 559.2f, 0.0f},
+
+      {0.0f, 0.0f, 0.0f, 0.0f},
+      {0.0f, 548.8f, 559.2f, 0.0f},
+      {0.0f, 0.0f, 559.2f, 0.0f},
+
+      // Left wall -- red lambert
+      {556.0f, 0.0f, 0.0f, 0.0f},
+      {556.0f, 0.0f, 559.2f, 0.0f},
+      {556.0f, 548.8f, 559.2f, 0.0f},
+
+      {556.0f, 0.0f, 0.0f, 0.0f},
+      {556.0f, 548.8f, 559.2f, 0.0f},
+      {556.0f, 548.8f, 0.0f, 0.0f},
+
+      // Short block -- white lambert
+      {130.0f, 165.0f, 65.0f, 0.0f},
+      {82.0f, 165.0f, 225.0f, 0.0f},
+      {242.0f, 165.0f, 274.0f, 0.0f},
+
+      {130.0f, 165.0f, 65.0f, 0.0f},
+      {242.0f, 165.0f, 274.0f, 0.0f},
+      {290.0f, 165.0f, 114.0f, 0.0f},
+
+      {290.0f, 0.0f, 114.0f, 0.0f},
+      {290.0f, 165.0f, 114.0f, 0.0f},
+      {240.0f, 165.0f, 272.0f, 0.0f},
+
+      {290.0f, 0.0f, 114.0f, 0.0f},
+      {240.0f, 165.0f, 272.0f, 0.0f},
+      {240.0f, 0.0f, 272.0f, 0.0f},
+
+      {130.0f, 0.0f, 65.0f, 0.0f},
+      {130.0f, 165.0f, 65.0f, 0.0f},
+      {290.0f, 165.0f, 114.0f, 0.0f},
+
+      {130.0f, 0.0f, 65.0f, 0.0f},
+      {290.0f, 165.0f, 114.0f, 0.0f},
+      {290.0f, 0.0f, 114.0f, 0.0f},
+
+      {82.0f, 0.0f, 225.0f, 0.0f},
+      {82.0f, 165.0f, 225.0f, 0.0f},
+      {130.0f, 165.0f, 65.0f, 0.0f},
+
+      {82.0f, 0.0f, 225.0f, 0.0f},
+      {130.0f, 165.0f, 65.0f, 0.0f},
+      {130.0f, 0.0f, 65.0f, 0.0f},
+
+      {240.0f, 0.0f, 272.0f, 0.0f},
+      {240.0f, 165.0f, 272.0f, 0.0f},
+      {82.0f, 165.0f, 225.0f, 0.0f},
+
+      {240.0f, 0.0f, 272.0f, 0.0f},
+      {82.0f, 165.0f, 225.0f, 0.0f},
+      {82.0f, 0.0f, 225.0f, 0.0f},
+
+      // Tall block -- white lambert
+
+      {423.0f, 330.0f + y_pos_param_init, 247.0f, 0.0f},
+      {265.0f, 330.0f + y_pos_param_init, 296.0f, 0.0f},
+      {314.0f, 330.0f + y_pos_param_init, 455.0f, 0.0f},
+
+      {423.0f, 330.0f + y_pos_param_init, 247.0f, 0.0f},
+      {314.0f, 330.0f + y_pos_param_init, 455.0f, 0.0f},
+      {472.0f, 330.0f + y_pos_param_init, 406.0f, 0.0f},
+
+      {423.0f, 0.0f + y_pos_param_init, 247.0f, 0.0f},
+      {423.0f, 330.0f + y_pos_param_init, 247.0f, 0.0f},
+      {472.0f, 330.0f + y_pos_param_init, 406.0f, 0.0f},
+
+      {423.0f, 0.0f + y_pos_param_init, 247.0f, 0.0f},
+      {472.0f, 330.0f + y_pos_param_init, 406.0f, 0.0f},
+      {472.0f, 0.0f + y_pos_param_init, 406.0f, 0.0f},
+
+      {472.0f, 0.0f + y_pos_param_init, 406.0f, 0.0f},
+      {472.0f, 330.0f + y_pos_param_init, 406.0f, 0.0f},
+      {314.0f, 330.0f + y_pos_param_init, 456.0f, 0.0f},
+
+      {472.0f, 0.0f + y_pos_param_init, 406.0f, 0.0f},
+      {314.0f, 330.0f + y_pos_param_init, 456.0f, 0.0f},
+      {314.0f, 0.0f + y_pos_param_init, 456.0f, 0.0f},
+
+      {314.0f, 0.0f + y_pos_param_init, 456.0f, 0.0f},
+      {314.0f, 330.0f + y_pos_param_init, 456.0f, 0.0f},
+      {265.0f, 330.0f + y_pos_param_init, 296.0f, 0.0f},
+
+      {314.0f, 0.0f + y_pos_param_init, 456.0f, 0.0f},
+      {265.0f, 330.0f + y_pos_param_init, 296.0f, 0.0f},
+      {265.0f, 0.0f + y_pos_param_init, 296.0f, 0.0f},
+
+      {265.0f, 0.0f + y_pos_param_init, 296.0f, 0.0f},
+      {265.0f, 330.0f + y_pos_param_init, 296.0f, 0.0f},
+      {423.0f, 330.0f + y_pos_param_init, 247.0f, 0.0f},
+
+      {265.0f, 0.0f + y_pos_param_init, 296.0f, 0.0f},
+      {423.0f, 330.0f + y_pos_param_init, 247.0f, 0.0f},
+      {423.0f, 0.0f + y_pos_param_init, 247.0f, 0.0f},
+
+      // Ceiling light -- emmissive
+      {343.0f, 548.6f, 227.0f, 0.0f},
+      {213.0f, 548.6f, 227.0f, 0.0f},
+      {213.0f, 548.6f, 332.0f, 0.0f},
+
+      {343.0f, 548.6f, 227.0f, 0.0f},
+      {213.0f, 548.6f, 332.0f, 0.0f},
+      {343.0f, 548.6f, 332.0f, 0.0f}}};
+
 static std::array<uint32_t, TRIANGLE_COUNT> g_mat_indices = {{
     0, 0,                         // Floor         -- white lambert
     0, 0,                         // Ceiling       -- white lambert
@@ -360,14 +499,14 @@ std::array<float3, MAT_COUNT> g_diffuse_colors_init =
       {0.05f, 0.80f, 0.05f},
       {0.80f, 0.05f, 0.05f},
       {0.50f, 0.00f, 0.00f},
-      {0.50f, 0.50f, 0.50f}}};
+      {0.10f, 0.30f, 0.95f}}};
 
 const std::array<bool, MAT_COUNT> g_material_parameter_mask =
     {{false,
       false,
       false,
       false,
-      true}};
+      false}};
 
 //------------------------------------------------------------------------------
 //
@@ -543,11 +682,11 @@ void buildMeshAccel(PathTracerState &state)
     //
     // copy mesh data to device
     //
-    const size_t vertices_size_in_bytes = g_vertices.size() * sizeof(Vertex);
+    const size_t vertices_size_in_bytes = g_vertices_gt.size() * sizeof(Vertex);
     CUDA_CHECK(cudaMalloc(reinterpret_cast<void **>(&state.d_vertices), vertices_size_in_bytes));
     CUDA_CHECK(cudaMemcpy(
         reinterpret_cast<void *>(state.d_vertices),
-        g_vertices.data(), vertices_size_in_bytes,
+        g_vertices_init.data(), vertices_size_in_bytes,
         cudaMemcpyHostToDevice));
 
     CUdeviceptr d_mat_indices = 0;
@@ -574,7 +713,7 @@ void buildMeshAccel(PathTracerState &state)
     triangle_input.type = OPTIX_BUILD_INPUT_TYPE_TRIANGLES;
     triangle_input.triangleArray.vertexFormat = OPTIX_VERTEX_FORMAT_FLOAT3;
     triangle_input.triangleArray.vertexStrideInBytes = sizeof(Vertex);
-    triangle_input.triangleArray.numVertices = static_cast<uint32_t>(g_vertices.size());
+    triangle_input.triangleArray.numVertices = static_cast<uint32_t>(g_vertices_gt.size());
     triangle_input.triangleArray.vertexBuffers = &state.d_vertices;
     triangle_input.triangleArray.flags = triangle_input_flags;
     triangle_input.triangleArray.numSbtRecords = MAT_COUNT;
